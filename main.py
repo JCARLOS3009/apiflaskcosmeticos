97b270f818ed5f6a99bcdfad5db4d8f70bb47cc3
JCARLOS3009 <85513751+JCARLOS3009@users.noreply.github.com>
2023-10-06 20:45:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, send_file
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_json():
+    # Especifique o caminho para o arquivo JSON externo
+    arquivo_json = 'test.json'  # Substitua pelo caminho real do seu arquivo JSON
+
+    try:
+        # Use send_file para enviar o arquivo JSON como resposta
+        return send_file(arquivo_json, mimetype='application/json')
+    except FileNotFoundError:
+        return "Arquivo não encontrado", 404
+
+app.run(host='0.0.0.0', port=81)

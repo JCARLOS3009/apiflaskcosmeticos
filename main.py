61068924bd8b1e60c7fb74bc677dadd2a98c6f34
JCARLOS3009 <85513751+JCARLOS3009@users.noreply.github.com>
2023-10-07 22:43:07 +0000

    1 commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def get_json():
     # Especifique o caminho para o arquivo JSON externo
-    arquivo_json = 'test.json'  # Substitua pelo caminho real do seu arquivo JSON
+    arquivo_json = 'cosmeticos.json'  # Substitua pelo caminho real do seu arquivo JSON
 
     try:
         # Use send_file para enviar o arquivo JSON como resposta
